84d9c39d36802dc2ae88296979cf10e4b46a6f84
aki <usamiaki5675@gmail.com>
2023-06-20 17:41:28 +0800

    init project

--- easyserver.go
+++ easyserver.go
@@ -19,13 +19,6 @@ import (
 // easyserver: framework
 // it has all of other servers needed things eg: tools middleware etc...
 
-// gameserver: players frame sync play etc..
-// roomserver: players matchmaking group stats etc...
-// relationserver: frend etc...
-// chatserver: chat
-// loginserver: login get token...
-// gateway: http -> grpc
-// notifyserver: tcp ---- client
 type IServer interface {
 	BeforeRun(*ServiceOpt)
 	Run(*ServiceOpt, grpc.ServiceRegistrar)
